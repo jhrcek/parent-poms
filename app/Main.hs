d2329b95856b692a3e3f9e49d5882e9434affe4e
Jan Hrcek <jhrcek@redhat.com>
2018-11-12 15:30:45 +0100

    WIP property usages

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import qualified Data.Map as Map
 import qualified Data.Set as Set
 import qualified Data.Text.IO as Text
 import qualified Maven.PomAncestors as Mvn
@@ -15,9 +16,18 @@ main = do
     ancestorChains <- Mvn.getAncestorChains
     gav2props <- Props.loadProperties userHome_ ancestorChains
 
-    let allOverrides = foldMap (Override.getOverrides gav2props) ancestorChains
-        uselessOverrides = Set.filter Override.isUseless allOverrides
+    let declared = mconcat $ Map.keysSet . Props.propsDeclared <$> Map.elems gav2props
+        used =  mconcat $ Props.propsUsed <$> Map.elems gav2props
+        unused = Set.difference declared used
+    putStrLn "===== Unused property declarations ====="
+    mapM_ print unused
 
+    let allOverrides = foldMap (Override.getOverrides gav2props) ancestorChains
+    putStrLn "===== All overrides ====="
+    mapM_ (Text.putStrLn . Override.formatOverride) allOverrides
+
+    let uselessOverrides = Set.filter Override.isUseless allOverrides
+    putStrLn "===== Useles overrides ====="
     mapM_ (Text.putStrLn . Override.formatOverride) uselessOverrides
 
     Graphviz.showHierarchy imageFormat_ nodeFormat_ ancestorChains gav2props
--- src/Pom/Properties.hs
+++ src/Pom/Properties.hs
@@ -5,28 +5,49 @@
 
 module Pom.Properties
     ( Properties
+    , DeclaredProps
+    , UsedProps
     , PropKey(PK)
     , PropValue(PV)
     , loadProperties
+    , propsDeclared
+    , propsUsed
     ) where
 
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
 import qualified Text.XML as XML
 
+import Data.List (nub)
 import Data.Map (Map)
 import Data.Maybe (fromMaybe)
+import Data.Set (Set)
 import Data.Text (Text)
-import Maven.Types (GAV(GAV), AncestorChain(AncestorChain))
-import Data.List (nub)
+import Maven.Types (AncestorChain (AncestorChain), GAV (GAV))
 import Prelude hiding (FilePath)
 import Text.XML (Document, Element, Name (..), def)
 import Text.XML.Lens (Traversal', el, localName, nodes, root, to, (./), (^.),
                       (^..), (^?), _Content, _Element, _head)
-import Turtle (FilePath, format, fp, (%), testfile, fromText, (</>))
+import Turtle (FilePath, format, fp, fromText, testfile, (%), (</>))
 
-type Properties = Map PropKey PropValue
+data Properties = Properties
+  { propsDeclared :: DeclaredProps
+  , propsUsed     :: UsedProps
+  }
+
+instance Semigroup Properties where
+    Properties d1 u1 <> Properties d2 u2 = Properties (d1 <> d2) (u1 <> u2)
+
+instance Monoid Properties where
+    mempty = Properties Map.empty Set.empty
+
+-- | Properties declared in the <properties> section of the pom.xml
+type DeclaredProps = Map PropKey PropValue
+-- | Properties used throughout the pom (stuff in ${..})
+type UsedProps = Set PropKey
+
 newtype PropKey = PK Text deriving newtype (Eq, Ord, Show)
 newtype PropValue = PV Text deriving newtype (Eq, Ord, Show)
 
@@ -42,28 +63,37 @@ loadProperties userHome ancestorChains =
         let pomPath = toPomPath userHome gav
         exists <- testfile pomPath
         if exists
-            then Map.singleton gav <$> readProperties pomPath
+            then do
+                declared <-  loadDeclaredProps pomPath
+                used <- loadUsedProps pomPath
+                return . Map.singleton gav $ Properties declared used
             else do
-              Text.putStrLn $ format ("WARNING: "%fp%" does not exist. Did you mvn install the repo?") pomPath
-              return Map.empty
+                Text.putStrLn $ format ("WARNING: "%fp%" does not exist. Did you mvn install the repo?") pomPath
+                return Map.empty
 
 toPomPath :: FilePath -> GAV -> FilePath
 toPomPath userHome (GAV g a v) =
     userHome </> fromText (Text.intercalate "/" [".m2/repository", Text.replace "." "/"  g, a, v, a <> "-" <> v <> ".pom"])
 
-{-| Read all property-value pairs from given pom.xml -}
-readProperties :: FilePath -> IO Properties
-readProperties pomFile =
-    parseProps <$> XML.readFile def ghcIoPomFile
+loadDeclaredProps :: FilePath -> IO DeclaredProps
+loadDeclaredProps pomFile =
+    parseDeclaredProps <$> XML.readFile def ghcIoPomFile
   where
     ghcIoPomFile = Text.unpack $ format fp pomFile
 
-parseProps :: Document -> Properties
-parseProps doc =
+loadUsedProps :: FilePath -> IO UsedProps
+loadUsedProps pomPath = do
+    pomContents <- Text.readFile $ Text.unpack $ format fp pomPath
+    case Text.splitOn "${" pomContents of
+        [] -> return Set.empty
+        (_:propsAndCrap) -> return . Set.fromList $ fmap (PK . Text.takeWhile (/='}')) propsAndCrap
+
+parseDeclaredProps :: Document -> DeclaredProps
+parseDeclaredProps doc =
     Map.fromList $ doc ^.. root
     . pomEl "project" ./ pomEl "properties"
     . nodes . traverse . _Element
-    . to getPropertyNameAndValue
+    . to getPropertyKeyAndValue
 
 pomEl :: Text -> Traversal' Element Element
 pomEl elName = el $ Name
@@ -72,8 +102,8 @@ pomEl elName = el $ Name
   , namePrefix = Nothing
   }
 
-getPropertyNameAndValue :: Element -> (PropKey, PropValue)
-getPropertyNameAndValue e = (PK key, PV val)
+getPropertyKeyAndValue :: Element -> (PropKey, PropValue)
+getPropertyKeyAndValue e = (PK key, PV val)
   where
     key =  e ^. localName
     val = fromMaybe "" $ e ^? nodes . _head . _Content
